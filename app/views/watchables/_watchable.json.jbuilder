02e4ba7039d9f914419d534d8a40beee024a62bb
locopati <andy.kriger@gmail.com>
2017-12-21 13:34:31 -0800

    Return season urls with watchable

--- app/views/watchables/_watchable.json.jbuilder
+++ app/views/watchables/_watchable.json.jbuilder
@@ -1,2 +1,8 @@
 json.merge! watchable.attributes
 json.url url_for(watchable)
+json.seasons do
+  json.array! watchable.episodes.map { |e| e.season_number }.uniq.sort do |season_number|
+    json.season_number season_number
+    json.url season_watchable_path(watchable_id: watchable.id, season_number: season_number)
+  end
+end
--- spec/requests/watchables_request_spec.rb
+++ spec/requests/watchables_request_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe 'Watchables requests' do
     expect(json.length).to eq watchable_count
     json.map {|w| expect(w['url']).to_not be_empty}
   end
+
+  it 'should return seasons for a watchable' do
+    watchable = create :watchable
+    season_count = 3
+    (1..season_count).each do |n|
+      create_list :episode, n, watchable: watchable, season_number: n
+    end
+    get "/watchables/#{watchable.id}", headers: {accept: 'application/json'}
+    expect(response).to render_template(:show)
+    json = JSON.parse(response.body)
+    seasons = json['seasons']
+    expect(seasons).to_not be_nil
+    expect(seasons.count).to eq season_count
+    seasons.each_with_index do |s, idx|
+      expect(s['season_number']).to eq idx+1
+      expect(s['url']).to_not be_empty
+    end
+  end
 end
