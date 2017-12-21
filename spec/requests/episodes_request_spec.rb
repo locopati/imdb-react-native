1a3716d2bf82e07175917143ac11835d3e9f8394
locopati <andy.kriger@gmail.com>
2017-12-21 12:18:58 -0800

    Added request specs

--- spec/requests/episodes_request_spec.rb
+++ spec/requests/episodes_request_spec.rb
@@ -1,45 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe EpisodesController do
+RSpec.describe 'Episodes requests' do
   it 'should return all episodes for a watchable' do
     episode_count = 10
     watchable =  create :watchable
     create_list :episode, episode_count, watchable: watchable
-    get :index, params: { watchable_id: watchable.id }
+    get "/watchables/#{watchable.id}/episodes", headers: {accept: 'application/json'}
+    expect(response).to render_template(:index)
     json = JSON.parse response.body
     expect(json.count).to eq episode_count
     # validate ordering by episode number
-    expect(json.map { |e| e['episode_number'].to_i }).to eq (1..10).to_a
-  end
-
-  it 'should return all episodes for a season' do
-    watchable = create :watchable
-    season_count = 3
-    create_season_episodes season_count, watchable
-    (1..season_count).each do |n|
-      get :season, params: { watchable_id: watchable.id, season_number: n }
-      json = JSON.parse response.body
-      expect(json.count).to eq n
-    end
-  end
-
-  it 'should return an episode within a season' do
-    watchable = create :watchable
-    season_count = 3
-    create_season_episodes season_count, watchable
-    (1..season_count).each do |season_num|
-      (1..season_num).each do |episode_num|
-        get :show, params: { watchable_id: watchable.id, season_number: season_num, episode_number: episode_num }
-        json = JSON.parse response.body
-        episode = Episode.where(watchable: watchable, season_number:  season_num, episode_num: episode_num)
-        json.each { |k, v| expect(v).to eq episode.send(k.to_sym) }
-      end
-    end
-  end
-
-  def create_season_episodes season_count, watchable
-    (1..season_count).each do |n|
-      create_list :episode, n, watchable: watchable, season_number: n
-    end
+    json.map { |e| expect(e['url']).to_not be_empty }
   end
 end
--- spec/requests/watchables_request_spec.rb
+++ spec/requests/watchables_request_spec.rb
@@ -1,7 +1,13 @@
-json = JSON.parse response.body
-expect(json.length).to eq(watchable_count)
+require 'rails_helper'
 
-json = JSON.parse response.body
-json.each do |k, v|
-  expect(v).to eq w.send(k.to_sym)
+RSpec.describe 'Watchables requests' do
+  it 'should get all watchables' do
+    watchable_count = 10
+    create_list :watchable, watchable_count
+    get '/watchables', headers: {accept: 'application/json'}
+    expect(response).to render_template(:index)
+    json = JSON.parse(response.body)
+    expect(json.length).to eq watchable_count
+    json.map {|w| expect(w['url']).to_not be_empty}
+  end
 end
